ea3e3db5422e5b61a9aca1fabfe5e65c34723070
Rodrigo Passos <92394086+rodrigohiray@users.noreply.github.com>
2023-10-19 20:45:21 +0100

    Exercicio
    Exercício concluído

--- app/src/main/java/com/example/projeto/MainActivity.kt
+++ app/src/main/java/com/example/projeto/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.projeto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.projeto.ui.theme.ProjetoTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,14 +36,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Row {
+        Column {
+            Text(text = "Rodrigo Passos")
+            Text(text = "2023/10/19")
+        }
+        Image(painter = painterResource(id = R.drawable.robo), contentDescription = "Logo", modifier = Modifier.size(49.dp).clip(
+            CircleShape))
+    }
 
 
-
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
 }
+
+
+
+
 
 @Preview(showBackground = true)
 @Composable
